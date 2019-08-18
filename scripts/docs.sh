96fb197db26fc03ad42e98c259e8227bced58c08
Ben Tilford <ben.tilford@bodybuilding.com>
2019-08-18 15:05:14 -0600

    Fix pre formatting

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -5,8 +5,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd $DIR/../
 cp README.md docs/index.md
 
-USAGE=`mocha --reporter=doc --grep="UriTemplate" --sort`
+mocha --reporter=doc --grep="UriTemplate" --sort > /tmp/uri-template-usage
 set +e
-echo $USAGE | tidy -config docs/.tidyrc > docs/_includes/usage.html
+cat /tmp/uri-template-usage | tidy -config docs/.tidyrc > docs/_includes/usage.html
 set -e
+rm /tmp/uri-template-usage
 cd $DIR
